ae8cbca7f50ea830e428055b59f0f342698390a5
Kevin Gut <kevin.gut@bitm.sg>
2019-03-20 03:11:46 +0100

    Convert into project and add exe properties

---
+++ SM3_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef SM3_PRIVATE_H
+#define SM3_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"2.0.0.0"
+#define VER_MAJOR	2
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"https://github.com/AyrA/SM3/"
+#define FILE_VERSION	"2.0.0.0"
+#define FILE_DESCRIPTION	"SM3 Hashing algorithm implementation"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"SM3.exe"
+#define PRODUCT_NAME	"SM3 hasher"
+#define PRODUCT_VERSION	"2.0.0.0"
+
+#endif /*SM3_PRIVATE_H*/
